97d1836381bee86f71f812703500823c30390384
24wings <2121718893@qq.com>
2017-02-18 21:05:37 +0800

    mongo

--- src/config.ts
+++ src/config.ts
@@ -3,13 +3,14 @@ export const Config = {
     // 项目文件夹
     ROOTDIR: path.resolve(__dirname, '../'),
     // 服务器运行端口
-    PORT: 3000,
+    PORT: 3001,
     // 静态文件服务器
     PUBLICDIR: path.resolve(__dirname, '../public'),
     //routes 文件夹,生成dist之后要改成dist/routes
     ROUTESDIR: path.resolve(__dirname + '/routes'),
     // mongoo数据库
-    MONGOURL: 'mongodb://localhost/test'
+    // MONGOURL: 'mongodb://localhost/test'
+    MONGOURL: 'mongodb://24wings:24wings@ds023664.mlab.com:23664/24wings'
 
 }
 
